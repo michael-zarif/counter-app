b3add2b2f149487e56ae22202275cf0f35290679
Michael Zarif <48383327+MichaelZarif@users.noreply.github.com>
2020-12-10 04:08:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
       { id: 1, value: 0 },
       { id: 2, value: 4 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 }
+      { id: 4, value: 0 }
     ],
     //tags: [],
   };
